9abb3116aaa54568017b7a79720bb620c4d6a26e
dahshan <>
2025-04-12 13:00:37 +0530

    layout-grid

--- src/components/content/home.tsx
+++ src/components/content/home.tsx
@@ -1,69 +1,98 @@
-import "ojs/ojinputtext";
 import { ojMessage } from "ojs/ojmessage";
 import "ojs/ojmessages";
 import { ojMessages } from "ojs/ojmessages";
 import { useCallback, useState } from "preact/hooks";
-
 export function Home() {
-        const isoTimeNow = new Date().toISOString();
-        const [Message, setMessage] = useState<ojMessage.Message[]>([]);
-        const [Display, setDisplay] = useState<'general' | 'notification'>("notification");
-        const position: ojMessages.Position = {
-                at: {
-                        horizontal: "end",
-                        vertical: "top",
-                },
-                my: {
-                        horizontal: "end",
-                        vertical: "top",
-                },               
-                 collision: 'flipfit'
-        };
+  const [messages, setmessages] = useState<ojMessage.Message[]>([]);
+  const [display, setdisplay] = useState<"notification" | "general">("notification");
+  const isoTimeNow = new Date().toISOString();
+  const position: ojMessages.Position = {
+    at: {
+      horizontal: "end",
+      vertical: "top",
+    },
+    my: {
+      horizontal: "start",
+      vertical: "top",
+    },
+    collision: "flipfit",
+  };
+  const onClickListner = () => {
+    setdisplay(display === "notification" ? "general" : "notification");
+    setmessages([
+      {
+        severity: "warning",
+        summary: "Error",
+        detail: "Error Message- detail",
+        autoTimeout: 1500,
+        timestamp: isoTimeNow,
+        icon: "./styles/images/avatar_24px.png",
+      },
+    ]);
+  };
+  const renderMessage = useCallback((item: ojMessages.MessageTemplateContext) => {
+    return (
+      <oj-message message={item.data} class="oj-color-invert">
+        <div slot="detail" class="oj-flex oj-sm-flex-direction-column">
+          <span class="oj-text-color-secondary oj-typography-body-sm">{item.data.detail}</span>
+        </div>
+      </oj-message>
+    );
+  }, []);
 
-        const handleClick = () => {
-                setDisplay(Display == "notification" ? "general" : "notification");
-                setMessage([
-                        {
-                                severity: "error",
-                                summary: "Error message summary",
-                                detail: "Error message detail",
-                                timestamp: isoTimeNow,
-                                sound: "default",
-                                autoTimeout: 2000,
-                                icon: "./styles/images/JET-Favicon-Red-32x32.png",
-                        },
-                ]);
-        };
+  return (
+    <div class="oj-web-applayout-max-width oj-web-applayout-content">
+      <h1>Home Page</h1>
+      <oj-button onClick={onClickListner}>oj-messages</oj-button>
+      <oj-messages id="messgaeid" messages={messages} position={position} display={display}>
+        <template slot="messageTemplate" render={renderMessage}></template>
+      </oj-messages>
+      <div class="oj-md-odd-cols-2 oj-md-even-cols-4">
+      <div class="oj-flex ">
+        <div class="oj-flex-item  "> 
+          <div class="oj-panel">
+            <h2>Ch1</h2>
+            <ol>
+              <li>Item 1</li>
+              <li>Item 2</li>
+              <li>Item 3</li>
+            </ol>
+          </div>
+        </div>
+        <div class="oj-flex-item  ">
+        <div class="oj-panel">
+            <h2>Ch2</h2>
+            <ol>
+              <li>Item 1</li>
+              <li>Item 2</li>
+              <li>Item 3</li>
+            </ol>
+          </div> </div>
+        <div class="oj-flex-item  " > 
+        <div class="oj-panel">
+            <h2>Ch3</h2>
+            <ol>
+              <li>Item 1</li>
+              <li>Item 2</li>
+              <li>Item 3</li>
+            </ol>
+          </div>
+        </div>
+        <div class="oj-flex-item  " > 
+        <div class="oj-panel">
+            <h2>Ch4</h2>
+            <ol>
+              <li>Item 1</li>
+              <li>Item 2</li>
+              <li>Item 3</li>
+            </ol>
+          </div>
+        </div>
+      </div>
+      </div>
 
-        const renderListItem = useCallback(
-          (item: ojMessages.MessageTemplateContext) => {
-            return (
-              <oj-message              
-              message={item.data}            
-              class="oj-color-invert">
-                <div slot="detail" class="oj-flex oj-sm-flex-direction-column">
-                  <span class="oj-text-color-secondary oj-typography-body-sm">
-                    {item.data.detail}
-                  </span>
-                </div>
-              </oj-message>
-            );
-          },
-          []
-        );
-
-        return (
-                <div class="oj-web-applayout-max-width oj-web-applayout-content">
-                        <h1>Home Page</h1>
-                        <oj-button id="button" onClick={handleClick}>
-                                message-notification
-                        </oj-button>
-                        <oj-messages id="message" messages={Message} position={position} display={Display}
-                       >
-                        <template slot="messageTemplate" render={renderListItem}>
-                        
-                        </template>
-                       </oj-messages>
-                </div>
-        );
+     
+     
+    </div>
+  );
 }
